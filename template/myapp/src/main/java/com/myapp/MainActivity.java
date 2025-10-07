6a15a74d6712a8fc5de0de09cee99b361c5d45a7
Cap-tain21 <captain21@example.com>
2025-10-07 14:05:15 +0530

    ci: add GitHub workflow to build Clicktoview APK

---
+++ template/myapp/src/main/java/com/myapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.myapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/webcontent/index.html");
+    }
+}
